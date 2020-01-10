dc7c36283c1b9fbd4db02efd68c0cbca68b32d3a
alamiotk <48948002+alamiotk@users.noreply.github.com>
2020-01-10 08:59:42 +0100

    Add files via upload

---
+++ DbModel.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uwp_App
+{
+    public class DbModel : DbContext
+    {
+
+      
+
+        public DbSet<Users> TRecepcjonista { get; set; }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+                => options.UseSqlite("Data Source=SystemV6.db");
+
+
+      
+    }
+}
---
+++ Recepcjonista.xaml.cs
@@ -0,0 +1,50 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace Uwp_App
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class Recepcjonista : Page
+    {
+        string login, haslo;
+        int id;
+
+
+
+        public Recepcjonista()
+        {
+            this.InitializeComponent();
+        }
+
+       
+
+        public Recepcjonista(string login, string haslo, int id)
+        {
+            this.login = login;
+            this.haslo = haslo;
+            this.id = id;
+        }
+
+
+
+    }
+}
