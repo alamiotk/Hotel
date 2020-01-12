ad1ace32f9df6fd3a43983165dc2a4521189d8df
aniawala <56352225+aniawala@users.noreply.github.com>
2020-01-12 14:25:35 +0100

    Add files via upload

---
+++ DbModel.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uwp_App
+{
+    public class DbModel : DbContext
+    {
+
+        
+        public DbSet<Rezerwacja> TRezerwacja { get; set; }
+        public DbSet<Meldunek> TMeldunki { get; set; }
+       
+
+        public DbSet<Users> TRecepcjonista { get; set; }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+                => options.UseSqlite("Data Source=SystemV6.db");
+
+
+      
+    }
+}
