7edd4bd9b9ce0dc7873cdb7eace3bbaa045da79b
aniawala <56352225+aniawala@users.noreply.github.com>
2020-01-12 14:19:01 +0100

    Add files via upload

---
+++ Meldunek.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uwp_App
+{
+    public class Meldunek
+    {
+        [Key]
+        public int nRezerwacji { get; set; }
+        public int nrKlucza { get; set; }
+        public bool czyZaplacono { get; set; }
+
+
+        public async void Zamelduj(int nRezerwacji, int nrKlucza)
+        {
+            this.nrKlucza = nrKlucza;            //klient zamedowany
+            this.nRezerwacji = nRezerwacji;
+            this.czyZaplacono = true;
+
+
+            await MSB.Print("Wydaj klucz do pokokju nr: " + nrKlucza);            // wydano klucz
+
+            using (var ctx = new DbModel())
+            {
+                ctx.TMeldunki.Add(this);
+                ctx.SaveChanges();
+                await MSB.Print("Wpisałem w baze");
+            }
+        }
+
+        public async void Wymelduj(int nRezerwacji, int nrKlucza)
+        {
+            using (var ctx = new DbModel())
+            {
+                //var ask = "SELECT nrKlucza FROM TMeldunek WHERE nRezerwacji == " + nrKlucza;
+                //var tmp = ctx.TMeldunki.FromSql(ask);
+                var tmp = ctx.TMeldunki.Where(a => a.nrKlucza == nrKlucza).ToArray();
+
+                if(tmp.Count() == 0)
+                {
+                    await MSB.Print("Nie wydano klucza nr: " + nrKlucza);
+                }
+                else
+                {
+                    if (nRezerwacji != tmp.FirstOrDefault().nRezerwacji)
+                    {
+                        await MSB.Print("Dla rezerwacji nr: " + nRezerwacji + " nie wydano klucza nr: " + nrKlucza);
+                        return;
+                    }
+                    ctx.Remove(tmp[0]);
+                    ctx.SaveChanges();
+                    await MSB.Print("Wypisałem z bazy");
+                }
+            }
+        }
+
+        public async void SprawdzPlatnosc(int nRezerwacji) // skoro ma zwracac void a -> to nieważne co tu się stanie, ale na sequencediagram_meldunek zwracany typ to void a w zależności od wyniku tej fukcji ma się wykonać część kodu mawet na diagramie jest to: zwraca void a a ma po bool u działać potem ??
+        {
+            var payment = new Platnosc();
+            var res = await payment.ZaplacAsync(nRezerwacji);
+            if (res)
+            {
+                //zaplacono
+                czyZaplacono = true; // zaznacz platnosc w systemie
+                Zamelduj(nRezerwacji, nrKlucza);
+            }
+            else
+            {
+                czyZaplacono = false; // skoro nie zaplacil to sie nie zamleowal czyli nie bd zapisaywal tej informacji nawet
+            }
+        }
+
+    }
+}
---
+++ Platnosc.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uwp_App
+{
+    class Platnosc
+    {
+        public async Task<bool> ZaplacAsync(int nRezerwacji)
+        {
+            // można by zapisać informacje o platnosci do bazy, ale sprawdz platnosc ma byc voidem czyli nie weryfikuje się tego, rozwniez z tego powodu nie ma mozliwosci przedplaty
+
+            // mozna w do...while dac ta funkcje i sprawdzac jak nacisnieto rezygnacje z platnosci warunek zeby wyjsc z petli
+
+            var res = await MSB.InputChoise("Wybierz metodę płatności", "Karta", "Gotówka");
+            // gdy jednoczenie wejdzie sie tutaj wyrzuci wyjatek ze peracja asnchronicznie nie została poprawnie rozpoczeta, // moj blad przypadkowy dwa razy wywolalem new meldunek.sprawdzplatnosc
+
+            switch (res)
+            {
+                // nieosiaglany kod: break;
+                case Windows.UI.Xaml.Controls.ContentDialogResult.None:
+                    {
+                        return false;
+                    }
+                case Windows.UI.Xaml.Controls.ContentDialogResult.Primary:
+                    {
+                        await Card();
+                        return true;
+                    }
+                case Windows.UI.Xaml.Controls.ContentDialogResult.Secondary:
+                    {
+                        await Cash();
+                        return true;
+                    }
+            }
+
+
+            return true; //zakladam że zapłacił
+        }
+
+        private async Task Card()
+        {
+            await MSB.Print("Postępuj według terminala"); // jakoś tak piszą.. na kasach
+        }
+
+        private async Task Cash()
+        {
+            await MSB.Print("Przyjmij gotówkę");
+
+            /*
+             * Najpierw firma płatnosci
+             * Obijasz kwotę na terminalie
+             * On placi
+             * Potem w komputerze w oknie klikasz
+             * Ale jeszcze musisz zapytać paragon czy fakrura
+             * 
+             * Wg. kolezanki
+             */
+        }
+
+    }
+}
