ceb9091b2c9dbe849ae2ca5d6234a34553f3d9b2
aniawala <56352225+aniawala@users.noreply.github.com>
2020-01-12 16:46:28 +0100

    Update Recepcjonista.xaml.cs

--- Recepcjonista.xaml.cs
+++ Recepcjonista.xaml.cs
@@ -36,7 +36,57 @@ namespace Uwp_App
         }
 
       
+         private async void Wymeldowanie_btn_Click(object sender, RoutedEventArgs e)
+        {
+            var nr = await MSB.PobierzNRezerwacjiAsync();
 
+            if (nr == 0)
+            {
+                return;
+            }
+
+            var nrKlucza = await MSB.PobierzNrAsync("klucza");
+
+            new Meldunek().Wymelduj(nr, nrKlucza);
+
+        }
+
+        private async void Zameldowanie_btn_Click(object sender, RoutedEventArgs e)
+        {
+            var nr = await MSB.PobierzNRezerwacjiAsync();
+
+            if (nr == 0)
+            {
+                return;
+            }
+
+            var nrKlucza = await MSB.PobierzNrAsync("klucza");
+
+            // sprawdz czy zameldowany
+            using (var ctx = new DbModel())
+            {
+                //var ask = "SELECT " + nr + " From TMeldunek";
+                //var tmp = ctx.TMeldunki.FromSql(ask);
+
+                var tmp = ctx.TMeldunki.Where(a => a.nrKlucza == nrKlucza).ToArray();
+
+                if (tmp.Count() != 0)
+                {
+                    if (tmp.FirstOrDefault().nrKlucza == nrKlucza)
+                    {
+                        // jestli dla danego nr rezerwacji jest wydany klujcz tzn. ze osoba jest juz zameldowana
+                        await MSB.Print("Dla rezerwacji: " + nr + " wydano już klucz nr: " + nrKlucza);
+                        return;
+                    }
+                } // wyrzuci błąd bo gdy nie ma nr klucza dla danego nr rezerwacji wartośc jest null em
+
+                else
+                {
+                    var meldunek = new Meldunek { nrKlucza = nrKlucza };
+                    meldunek.SprawdzPlatnosc(nr);
+                }
+            }
+        }
      
 
 
