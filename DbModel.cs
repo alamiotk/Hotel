ba54478fb217dc3c89d60ff73684f4205e6ca316
adubiel7 <57836236+adubiel7@users.noreply.github.com>
2020-01-13 01:37:43 +0100

    Update DbModel.cs

--- DbModel.cs
+++ DbModel.cs
@@ -15,8 +15,6 @@ namespace Uwp_App
         public DbSet<Rezerwacja> TRezerwacja { get; set; }
         public DbSet<Meldunek> TMeldunki { get; set; }
         public DbSet<Atrakcje> TAtrakcje { get; set; }
-        public DbSet<Basen> TBasen { get; set; }
-        public DbSet<Spa> TSpa { get; set; }
 
         public DbSet<Users> TRecepcjonista { get; set; }
 
